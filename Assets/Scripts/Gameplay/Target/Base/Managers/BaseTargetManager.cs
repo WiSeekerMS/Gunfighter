e6918a6bbf506e2028eec9281a9786ccfa2c167a
WiSeeker <maxsim-troizk@yandex.ru>
2023-12-21 01:18:33 +0500

    Commit

---
+++ Assets/Scripts/Gameplay/Target/Base/Managers/BaseTargetManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Common.Audio;
+using Gameplay.ShotSystem.Signals;
+using Gameplay.Target.Common.View;
+using UnityEngine;
+using Zenject;
+
+namespace Gameplay.Target.Base.Managers
+{
+    public abstract class BaseTargetManager : MonoBehaviour
+    {
+        [SerializeField] protected List<AudioClip> _clips;
+        [SerializeField] protected List<RandomTargetView> _targetViews;
+        [Inject] protected SignalBus _signalBus;
+        [Inject] protected AudioController _audioController;
+        
+        private void Awake()
+        {
+            _signalBus.Subscribe<ShotSignals.Hit>(OnHit);
+        }
+
+        private void OnDestroy()
+        {
+            _signalBus.Unsubscribe<ShotSignals.Hit>(OnHit);
+        }
+
+        protected abstract void OnHit(ShotSignals.Hit signal);
+    }
+}
---
+++ Assets/Scripts/Gameplay/Target/GlassBottle/Managers/GlassBottlesManager.cs
@@ -0,0 +1,23 @@
+﻿using Gameplay.ShotSystem.Signals;
+using Gameplay.Target.Base.Managers;
+using Gameplay.Target.Common.View;
+
+namespace Gameplay.Target.GlassBottle.Managers
+{
+    public class GlassBottlesManager : BaseTargetManager
+    {
+        protected override void OnHit(ShotSignals.Hit signal)
+        {
+            var component = signal.HitInfo.transform.GetComponent<RandomTargetView>();
+            if (!component) return;
+
+            var bottle = _targetViews.Find(g => g.Equals(component));
+            if (!bottle) return;
+
+            var point = signal.HitInfo.point;
+            bottle.OnHit(point);
+            
+            _audioController.PlayRandomAudioClip(_clips, point);
+        }
+    }
+}
